1dc9aa257329123308a26ba8e123cabad8c35c0e
James Doc <hi@jamesdoc.com>
2020-07-07 21:48:47 +0100

    Tweak asset generation

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -2,7 +2,7 @@ module.exports = {
 
   svgIcon: (icon, cls) => {
     return `<svg class="${cls}">
-      <use xlink:href="/svg/sprite.svg#${icon}"></use>
+      <use xlink:href="/assets/svg/sprite.svg#${icon}"></use>
     </svg>`;
   }
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,12 +4,12 @@ const svgSprite = require('@ayctor/laravel-mix-svg-sprite');
 
 // Run mix
 mix
-  .setPublicPath(`dist`)
+  .setPublicPath(`dist/assets`)
   .sourceMaps()
-  .postCss('./src/_assets/main.css', 'assets', [
+  .postCss('./src/_assets/main.css', '', [
     require('tailwindcss'),
   ])
-  .js('./src/_assets/main.js', 'assets')
+  .js('./src/_assets/main.js', '')
   .extract()
   .minify([
     './dist/assets/vendor.js',
@@ -17,6 +17,6 @@ mix
     './dist/assets/main.js',
     './dist/assets/main.css'
   ])
-  .svgSprite('./src/_assets/svg/**/*.svg', 'assets/sprite.svg')
+  .svgSprite('./src/_assets/svg/**/*.svg', 'sprite.svg')
   .version();
 
